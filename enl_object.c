ebaa2e4fcf0bda500168c586554b7b067ab32a7a
ryuever <liuyouchao111@gmail.com>
2014-04-02 23:56:18 +0900

    format

--- enl_object.c
+++ enl_object.c
@@ -13,40 +13,36 @@
 enl_object_list* dev_obj_list = NULL;
 enl_object_list* prof_obj_list = NULL;
 
-int enl_add_obj_to_list(enl_object* eoj)
-{
+int enl_add_obj_to_list(enl_object* eoj){
+
 	enl_object* head = dev_obj_list->eoj;
-	if(dev_obj_list->eoj == NULL)
-	{
+	if(dev_obj_list->eoj == NULL){
 		dev_obj_list->eoj = eoj;
 		//head->next = NULL;
-	}
-	else
-	{
+	}else{
 		enl_object* next_obj = head;
 		head = eoj;
 		head->next = next_obj;
 		enl_object* headnext = head->next;
 		headnext->prev = head;
 	}
+    return 0;
 }
 
-int enl_find_obj_in_list(unsigned char class_group_code, unsigned char class_code, unsigned char instance_code)
-{
-	if((dev_obj_list == NULL) || (dev_obj_list->eoj == NULL))
-	{
+int enl_find_obj_in_list(unsigned char class_group_code, 
+                         unsigned char class_code, 
+                         unsigned char instance_code){
+	if((dev_obj_list == NULL) || (dev_obj_list->eoj == NULL)){
 		return -1;
 	}
 
 	enl_object* head = dev_obj_list->eoj;
 	enl_object* p = head;
 	while((p->cls_gcode != class_group_code) &&
-	        (p->cls_code != class_code) &&
-	        (p->ins_code != instance_code))
-	{
+          (p->cls_code != class_code) &&
+          (p->ins_code != instance_code)){
 		p = p->next;
-		if(p == NULL)
-		{
+		if(p == NULL){
 			return -1;
 		}
 	}
@@ -54,18 +50,19 @@ int enl_find_obj_in_list(unsigned char class_group_code, unsigned char class_cod
 	return 0;
 }
 
-int enl_create_eoj(unsigned char obj_type, unsigned char class_group_code, unsigned char class_code, unsigned char instance_code, unsigned int* eoj_code)
-{
+int enl_create_eoj(unsigned char obj_type, 
+                   unsigned char class_group_code, 
+                   unsigned char class_code, 
+                   unsigned char instance_code, 
+                   unsigned int* eoj_code){
 	enl_object* eoj = NULL;
 
-	if(dev_obj_list == NULL)
-	{
+	if(dev_obj_list == NULL){
 		dev_obj_list = enl_malloc(sizeof(enl_object_list));
 		dev_obj_list->eoj = NULL;
 	}
 
-	if(0 == enl_find_obj_in_list(class_group_code, class_code, instance_code))
-	{
+	if(0 == enl_find_obj_in_list(class_group_code, class_code, instance_code)){
 		//object existed
 		return 0;
 	}
@@ -82,10 +79,12 @@ int enl_create_eoj(unsigned char obj_type, unsigned char class_group_code, unsig
 	enl_add_obj_to_list(eoj);
 
 	*eoj_code = enl_convert_eoj_code(class_group_code, class_code, instance_code);
+    return 0;
 }
 
-unsigned int enl_convert_eoj_code(unsigned char class_group_code, unsigned char class_code, unsigned char instance_code)
-{
+unsigned int enl_convert_eoj_code(unsigned char class_group_code, 
+                                  unsigned char class_code, 
+                                  unsigned char instance_code){
 	unsigned int ret = 0;
 	unsigned int* p = &ret;
 
@@ -102,8 +101,10 @@ unsigned int enl_convert_eoj_code(unsigned char class_group_code, unsigned char 
 	return ret;
 }
 
-void enl_split_eoj_code(unsigned int eoj_code, unsigned char* class_group_code, unsigned char* class_code, unsigned char* instance_code)
-{
+void enl_split_eoj_code(unsigned int eoj_code, 
+                        unsigned char* class_group_code, 
+                        unsigned char* class_code, 
+                        unsigned char* instance_code){
 	unsigned int* p = &eoj_code;
 
 	unsigned char* b0 = (unsigned char*)p;
@@ -115,14 +116,12 @@ void enl_split_eoj_code(unsigned int eoj_code, unsigned char* class_group_code, 
 	*instance_code = *b2;
 }
 
-enl_object* enl_get_object_location(unsigned int eoj_code)
-{
+enl_object* enl_get_object_location(unsigned int eoj_code){
 	unsigned char class_group_code = 0;
 	unsigned char class_code = 0;
 	unsigned char instance_code = 0;
 
-	if((dev_obj_list == NULL) || (dev_obj_list->eoj == NULL))
-	{
+	if((dev_obj_list == NULL) || (dev_obj_list->eoj == NULL)){
 		return NULL;
 	}
 
@@ -131,37 +130,36 @@ enl_object* enl_get_object_location(unsigned int eoj_code)
 	enl_object* head = dev_obj_list->eoj;
 	enl_object* p = head;
 	while((p) &&
-		    (p->cls_gcode != class_group_code) &&
-	        (p->cls_code != class_code) &&
-	        (p->ins_code != instance_code))
-	{
+          (p->cls_gcode != class_group_code) &&
+          (p->cls_code != class_code) &&
+          (p->ins_code != instance_code)){
 		p = p->next;
 	}
 
-	if(p == NULL)
-	{
+	if(p == NULL){
 		return NULL;
 	}
 
 	return p;
 }
 
-int enl_get_property_status(unsigned int eoj_code, unsigned char epc, unsigned char* pdc, unsigned char** edt, unsigned char* status, unsigned char* access_rule)
-{
+int enl_get_property_status(unsigned int eoj_code, 
+                            unsigned char epc, 
+                            unsigned char* pdc, 
+                            unsigned char** edt, 
+                            unsigned char* status, 
+                            unsigned char* access_rule){
 	enl_object* obj = enl_get_object_location(eoj_code);
-	if((obj == NULL) || (obj->prop == NULL))
-	{
+	if((obj == NULL) || (obj->prop == NULL)){
 		*status = STATUS_PROP_NOT_EXISTED;
 		return -1;
 	}
 
 	enl_object_prop* head = obj->prop;
 	enl_object_prop* p = head;
-	while(p->epc != epc)
-	{
+	while(p->epc != epc){
 		p = p->next;
-		if(p == NULL)
-		{
+		if(p == NULL){
 			*status = STATUS_PROP_NOT_EXISTED;
 			return -1;
 		}
@@ -176,11 +174,13 @@ int enl_get_property_status(unsigned int eoj_code, unsigned char epc, unsigned c
 	return 0;
 }
 
-int enl_set_property_status(unsigned int eoj_code, unsigned char epc, unsigned char pdc, unsigned char* edt, unsigned char* status)
-{
+int enl_set_property_status(unsigned int eoj_code, 
+                            unsigned char epc, 
+                            unsigned char pdc, 
+                            unsigned char* edt, 
+                            unsigned char* status){
 	enl_object* obj = enl_get_object_location(eoj_code);
-	if(obj == NULL)
-	{
+	if(obj == NULL){
 		*status = STATUS_OBJ_NOT_EXISTED;
 		return -1;
 	}
@@ -188,21 +188,17 @@ int enl_set_property_status(unsigned int eoj_code, unsigned char epc, unsigned c
 	enl_object_prop* head = obj->prop;
 	enl_object_prop* p = head;
 
-	if(head == NULL)
-	{
+	if(head == NULL){
 		head = (enl_object_prop*)enl_malloc(sizeof(enl_object_prop));
 		head->epc = epc;
 		head->pdc = pdc;
 		head->edt = (unsigned char*)enl_malloc(pdc);
 		memcpy(head->edt, edt, pdc);
 	}
-	else
-	{
-		while(p->epc != epc)
-		{
+	else{
+		while(p->epc != epc){
 			p = p->next;
-			if(p == NULL)
-			{
+			if(p == NULL){
 				enl_object_prop* next_prop = head;
 				head = (enl_object_prop*)enl_malloc(sizeof(enl_object_prop));
 				head->epc = epc;
@@ -216,8 +212,7 @@ int enl_set_property_status(unsigned int eoj_code, unsigned char epc, unsigned c
 		}
 
 		p->epc = epc;
-		if(p->pdc != pdc)
-		{
+		if(p->pdc != pdc){
 			enl_free(p->edt);
 			p->edt = (unsigned char*)enl_malloc(pdc);
 		}
@@ -228,11 +223,14 @@ int enl_set_property_status(unsigned int eoj_code, unsigned char epc, unsigned c
 	return 0;
 }
 
-int enl_set_property(unsigned int eoj_code, unsigned char epc, unsigned char pdc, unsigned char* edt, unsigned char* status, unsigned char access_rule)
-{
+int enl_set_property(unsigned int eoj_code, 
+                     unsigned char epc, 
+                     unsigned char pdc, 
+                     unsigned char* edt, 
+                     unsigned char* status, 
+                     unsigned char access_rule){
 	enl_object* obj = enl_get_object_location(eoj_code);
-	if(obj == NULL)
-	{
+	if(obj == NULL){
 		*status = STATUS_OBJ_NOT_EXISTED;
 		return -1;
 	}
@@ -240,8 +238,7 @@ int enl_set_property(unsigned int eoj_code, unsigned char epc, unsigned char pdc
 	enl_object_prop* head = obj->prop;
 	enl_object_prop* p = head;
 
-	if(obj->prop == NULL)
-	{
+	if(obj->prop == NULL){
 		enl_object_prop* ptemp = (enl_object_prop*)enl_malloc(sizeof(enl_object_prop));
 		obj->prop = ptemp;
 		head = obj->prop;
@@ -251,13 +248,10 @@ int enl_set_property(unsigned int eoj_code, unsigned char epc, unsigned char pdc
 		head->edt = (unsigned char*)enl_malloc(pdc);
 		memcpy(head->edt, edt, pdc);
 	}
-	else
-	{
-		while(p->epc != epc)
-		{
+	else{
+		while(p->epc != epc){
 			p = p->next;
-			if(p == NULL)
-			{
+			if(p == NULL){
 				enl_object_prop* next_prop = head;
 				head = (enl_object_prop*)enl_malloc(sizeof(enl_object_prop));
 				head->access_rule = access_rule;
@@ -272,8 +266,7 @@ int enl_set_property(unsigned int eoj_code, unsigned char epc, unsigned char pdc
 		}
 
 		p->epc = epc;
-		if(p->pdc != pdc)
-		{
+		if(p->pdc != pdc){
 			enl_free(p->edt);
 			p->edt = (unsigned char*)enl_malloc(pdc);
 		}
@@ -285,23 +278,18 @@ int enl_set_property(unsigned int eoj_code, unsigned char epc, unsigned char pdc
 }
 
 
-int enl_get_object_num(unsigned char* edt_size)
-{
+int enl_get_object_num(unsigned char* edt_size){
 	unsigned char obj_num = 0;
-	if((dev_obj_list == NULL) || (dev_obj_list->eoj == NULL))
-	{
+	if((dev_obj_list == NULL) || (dev_obj_list->eoj == NULL)){
 		*edt_size = 0;
 		return 0;
 	}
 
 	enl_object* p = dev_obj_list->eoj;
-	while(p != NULL)
-	{
-		if(p->type == 0) //device object
-		{
+	while(p != NULL){
+		if(p->type == 0){
 			obj_num++;
-			if(obj_num == 255)
-			{
+			if(obj_num == 255){
 				*edt_size = 255;
 				return 0;
 			}
@@ -314,8 +302,7 @@ int enl_get_object_num(unsigned char* edt_size)
 	return 0;
 }
 
-int enl_get_object_list(unsigned char* edt_size, unsigned char** edt)
-{
+int enl_get_object_list(unsigned char* edt_size, unsigned char** edt){
 	unsigned char obj_num = 0;
 	unsigned char edt_size_real = 0;
 	unsigned char* pedt = NULL;
@@ -324,8 +311,7 @@ int enl_get_object_list(unsigned char* edt_size, unsigned char** edt)
 	unsigned char* pos3 = NULL;
 
 	enl_get_object_num(&edt_size_real);
-	if(edt_size_real == 0)
-	{
+	if(edt_size_real == 0){
 		*edt_size = 0;
 		return 0;
 	}
@@ -333,15 +319,14 @@ int enl_get_object_list(unsigned char* edt_size, unsigned char** edt)
 	obj_num = (edt_size_real >= 84) ? 84 : edt_size_real;
 	*edt_size = 1+ obj_num * 3;
 
-	pedt = (unsigned char*)enl_malloc(*edt_size);
+	pedt = enl_malloc(*edt_size);
 	*edt = pedt;
 	//pedt = *edt;
 	//*pedt = obj_num;
 	obj_num = 0;
 
 	enl_object* p = dev_obj_list->eoj;
-	while(p != NULL)
-	{
+	while(p != NULL){
 		obj_num++;
 		pos1 = (unsigned char*)(pedt + 1 + (obj_num - 1) * 3);
 		pos2 = (unsigned char*)(pedt + 2 + (obj_num - 1) * 3);
@@ -349,8 +334,7 @@ int enl_get_object_list(unsigned char* edt_size, unsigned char** edt)
 		*pos1 = p->cls_gcode;
 		*pos2 = p->cls_code;
 		*pos3 = p->ins_code;
-		if(obj_num == 84)
-		{
+		if(obj_num == 84){
 			return 0;
 		}
 
@@ -361,12 +345,10 @@ int enl_get_object_list(unsigned char* edt_size, unsigned char** edt)
 	return 0;
 }
 
-int enl_get_class_num(unsigned char* edt_size, unsigned char** edt)
-{
+int enl_get_class_num(unsigned char* edt_size, unsigned char** edt){
 	return 0;
 }
 
-int enl_get_class_list(unsigned char* edt_size, unsigned char** edt)
-{
+int enl_get_class_list(unsigned char* edt_size, unsigned char** edt){
 	return 0;
 }
